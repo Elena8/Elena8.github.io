5150aab56e4510fed447f5c0120c4f1d8cb1e7a9
Elena8 <mol4anova_@mail.ru>
2017-02-17 22:29:25 +0300

    create Cult1 and add timer

--- Cult1/js/script.js
+++ Cult1/js/script.js
@@ -16,6 +16,53 @@ $(function(){
     
    $( "#datepicker" ).datepicker();
     
+    $("td").click(function(){
+        console.log("dfhvhdgfhgdfh");
+        console.log($(this).text());
+        console.log($(this).attr("data-month"));
+        console.log($(this).attr("data-year"));
+    });
+    
+   /* $(document).ready(function(){
+			$("#countdown").countdown({
+				date: "01 april 2017 12:00:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+		});*/
+    
+    var note = $('#note'),
+                ts = new Date(2017, 3, 1),
+                newYear = true;
+        if((new Date()) > ts){
+                // The new year is here! Count towards something else.
+                // Notice the *1000 at the end - time must be in milliseconds
+                ts = (new Date()).getTime() + 10*24*60*60*1000;
+                newYear = false;
+        }
+        $('#countdown').countdown({
+                timestamp        : ts,
+                callback        : function(days, hours, minutes, seconds){
+                        var message = "";
+                        message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+                        message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+                        message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+                        message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+                        if(newYear){
+                                message += "left until the new year!";
+                        }
+                        else {
+                                message += "left to 10 days from now!";
+                        }
+                        note.html(message);
+                }
+        });
+    
+    
+    
 });
 /*$(window).load(function() {
   $('.flexslider').flexslider({
