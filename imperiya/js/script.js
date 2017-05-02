0006f1842b9167be068e4216bc9617c6b8302bc0
Elena8 <mol4anova_@mail.ru>
2017-05-02 22:39:36 +0300

    add  pages

--- imperiya/js/script.js
+++ imperiya/js/script.js
@@ -1,16 +1,68 @@
  $(function() {
 
     $('.flexslider').flexslider({
-
-          animation: "fade"
-
+        animation: "fade"
     });
      
+//////////////////////////////////////////
+     
      $(document).on('click',function(){
-    $('.collapse').collapse('hide');
+        $('.collapse').collapse('hide');
+    });
+     
+/////////////////////////////////////////
+         
+         // блок карусели
+	var carousel = $("#carousel");
+     carousel.owlCarousel({
+	// Количество отображающихся блоков 
+	// в зависимости от устройства (ширины экрана)
+	
+	// Количество блоков на больших экранах
+	items:             10,
+	
+	// 5 блоков на компьютерах (экран от 1400px до 901px)
+	itemsDesktop:      [1400, 5],
+	
+	// 3 блока на маленьких компьютерах (экран от 900px до 601px)
+	itemsDesktopSmall: [900, 3],
+	
+	// 2 элемента на планшетах (экран от 600 до 480 пикселей)
+	itemsTablet:       [600, 2],
+	
+	// Настройки для телефона отключены, в этом случае будут
+	// использованы настройки планшета
+	itemsMobile:       [479, 1],
+         //Базовая скорость
+    slideSpeed : 200,
+    paginationSpeed : 800,
+    rewindSpeed : 1000,
+     
+    //Авто-прокрутка
+    autoPlay : true,
+    stopOnHover : false,
+     
+    // Навигация
+    navigation : false,
+    navigationText : ["prev","next"],
+    rewindNav : true,
+    scrollPerPage : false,
+     
+    // Пагинация
+    pagination : true,
+    paginationNumbers: false,
+     
+    // Отзывчивость
+    responsive: true,
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window,
+
+
   });
 
- 
+
+	
+	
 
     $(function() {
 
